cb7ce9b172378f917c70fcee2fe080a656b4af76
Tommaso <tomm.vitale@gmail.com>
2020-10-29 12:12:15 +0100

    Update datatemper-log.py

--- datatemper-log.py
+++ datatemper-log.py
@@ -4,7 +4,7 @@
 
 # TODO: optimize code | ...
 
-# Copyright (C) 2019  tomvitale
+# Copyright (C) 2020  tomvitale
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
